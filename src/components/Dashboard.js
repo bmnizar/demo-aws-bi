42e7d778c8609f7503d8dde7c4e4439df0837052
bmnizar <nizar.ben.mansour@gmail.com>
2022-05-26 13:25:06 +0200

    Initial commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,142 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Menu } from 'primereact/menu';
+import { Button } from 'primereact/button';
+import { Chart } from 'primereact/chart';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { ProductService } from '../service/ProductService';
+
+const lineData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            backgroundColor: '#2f4860',
+            borderColor: '#2f4860',
+            tension: .4
+        },
+        {
+            label: 'Second Dataset',
+            data: [28, 48, 40, 19, 86, 27, 90],
+            fill: false,
+            backgroundColor: '#00bb7e',
+            borderColor: '#00bb7e',
+            tension: .4
+        }
+    ]
+};
+
+const Dashboard = (props) => {
+    const [products, setProducts] = useState(null);
+    const menu1 = useRef(null);
+    const menu2 = useRef(null);
+    const [lineOptions, setLineOptions] = useState(null)
+
+    const applyLightTheme = () => {
+        const lineOptions = {
+            plugins: {
+                legend: {
+                    labels: {
+                        color: '#495057'
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef',
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: '#495057'
+                    },
+                    grid: {
+                        color: '#ebedef',
+                    }
+                },
+            }
+        };
+
+        setLineOptions(lineOptions)
+    }
+
+    const applyDarkTheme = () => {
+        const lineOptions = {
+            plugins: {
+                legend: {
+                    labels: {
+                        color: '#ebedef'
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: '#ebedef'
+                    },
+                    grid: {
+                        color: 'rgba(160, 167, 181, .3)',
+                    }
+                },
+                y: {
+                    ticks: {
+                        color: '#ebedef'
+                    },
+                    grid: {
+                        color: 'rgba(160, 167, 181, .3)',
+                    }
+                },
+            }
+        };
+
+        setLineOptions(lineOptions)
+    }
+
+    useEffect(() => {
+        const productService = new ProductService();
+        productService.getProductsSmall().then(data => setProducts(data));
+    }, []);
+
+    useEffect(() => {
+        if (props.colorMode === 'light') {
+            applyLightTheme();
+        } else {
+            applyDarkTheme();
+        }
+    }, [props.colorMode]);
+
+    const formatCurrency = (value) => {
+        return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    };
+
+    return (
+       
+                <div className="card">
+                    <h5>Recent Sales</h5>
+                    <DataTable value={products} rows={5} paginator responsiveLayout="scroll">
+                        <Column header="Image" body={(data) => <img className="shadow-2" src={`assets/demo/images/product/${data.image}`} alt={data.image} width="50"/>}/>
+                        <Column field="name" header="Name" sortable style={{width: '35%'}}/>
+                        <Column field="price" header="Price" sortable style={{width: '35%'}} body={(data) => formatCurrency(data.price)}/>
+                        <Column header="View" style={{width:'15%'}} body={() => (
+                            <>
+                                <Button icon="pi pi-search" type="button" className="p-button-text"/>
+                            </>
+                        )}/>
+                                     
+                    </DataTable> 
+                </div>
+                 
+    );
+}
+
+const comparisonFn = function (prevProps, nextProps) {
+    return (prevProps.location.pathname === nextProps.location.pathname) && (prevProps.colorMode === nextProps.colorMode);
+};
+
+export default React.memo(Dashboard, comparisonFn);
